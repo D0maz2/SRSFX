5d22bfdbddd9dd5329a50566ffcfabc49e60e904
D0ma2 <adham.mohamed.elmahdy@gmail.com>
2023-05-11 23:56:53 +0300

    Adham

--- src/main/java/com/example/srsfx/loginController.java
+++ src/main/java/com/example/srsfx/loginController.java
@@ -4,8 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class loginController {
-    @FXML
-    protected void onHelloButtonClick() {
+//    @FXML    is used when some component is given an fx:id and is declared as a private attribute/method and then we need the @FXML 2o inject the component correctly
 
-    }
 }
---
+++ src/main/java/com/example/srsfx/registerScreen.java
@@ -0,0 +1,5 @@
+package com.example.srsfx;
+
+public class registerScreen {
+
+}
