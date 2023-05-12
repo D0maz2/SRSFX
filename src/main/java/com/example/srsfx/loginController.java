965466abeacbf89ae0d01efe90d3e1e45c9350c0
D0ma2 <adham.mohamed.elmahdy@gmail.com>
2023-05-13 00:52:21 +0300

    Adham

--- src/main/java/com/example/srsfx/loginController.java
+++ src/main/java/com/example/srsfx/loginController.java
@@ -1,9 +1,31 @@
 package com.example.srsfx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class loginController {
 //    @FXML    is used when some component is given an fx:id and is declared as a private attribute/method and then we need the @FXML 2o inject the component correctly
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchToRegister(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(loginScreen.class.getResource("registerScreen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 800, 450);
+        stage.setResizable(false);
+        stage.setTitle("SRS Register");
+        stage.getIcons().add(new Image(registerScreen.class.getResourceAsStream("/thumbnail.jpg")));
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
