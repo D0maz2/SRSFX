0af4506a6b55a489362eb659c4e137bdea935108
Abdelrahman Malavi <52723306+AJlGnd@users.noreply.github.com>
2023-05-16 20:54:19 +0300

    Add files via upload

---
+++ src/main/java/com/example/srsfx/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.srsfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeController {
+    @FXML
+    ImageView profileid;
+
+
+    public void cancelOnclick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(loginScreen.class.getResource("loginScreen.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 800, 450);
+        stage.setResizable(false);
+        stage.setTitle("SRS Login");
+        stage.getIcons().add(new Image(registerController.class.getResourceAsStream("/thumbnail.jpg")));
+        stage.setScene(scene);
+        stage.show();
+    }
+}
