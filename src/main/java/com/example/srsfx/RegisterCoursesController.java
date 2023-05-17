af43d88cb61c7cdf74446b2e0b698326b03d4b89
D0maz2 <114911810+D0maz2@users.noreply.github.com>
2023-05-17 22:36:49 +0300

    Add files via upload
    past commits crashed trying to fix :)

--- src/main/java/com/example/srsfx/RegisterCoursesController.java
+++ src/main/java/com/example/srsfx/RegisterCoursesController.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import mainClasses.Course;
 import mainClasses.Department;
 import mainClasses.Student;
@@ -13,6 +14,22 @@ import java.util.ArrayList;
 public class RegisterCoursesController {
     @FXML
     ComboBox cboxTerm;
+    @FXML
+    TableColumn tcCourseName;
+    @FXML
+    TableColumn tcCourseID;
+    @FXML
+    TableColumn tcDay;
+    @FXML
+    TableColumn tcPeriod;
+    @FXML
+    TableColumn tcClassroom;
+    @FXML
+    TableColumn tcInstructor;
+    @FXML
+    TableColumn tcCredits;
+    @FXML
+    TableColumn tcSeats;
     ObservableList<String> list = FXCollections.observableArrayList();
     ArrayList<Course> allCourses = new ArrayList<Course>();
     ArrayList<Course> registerableCourses = new ArrayList<Course>();
@@ -26,6 +43,24 @@ public class RegisterCoursesController {
         list.add("Seventh");
         list.add("Eighth");
         cboxTerm.setItems(list);
+
+        tcCourseName.setSortable(false);
+        tcCourseName.setResizable(false);
+        tcCourseID.setSortable(false);
+        tcCourseID.setResizable(false);
+        tcDay.setSortable(false);
+        tcDay.setResizable(false);
+        tcPeriod.setSortable(false);
+        tcPeriod.setResizable(false);
+        tcClassroom.setSortable(false);
+        tcClassroom.setResizable(false);
+        tcInstructor.setSortable(false);
+        tcInstructor.setResizable(false);
+        tcCredits.setSortable(false);
+        tcCredits.setResizable(false);
+        tcSeats.setSortable(false);
+        tcSeats.setResizable(false);
+
     }
 
     public void filterCourses(Student S1){
@@ -38,7 +73,6 @@ public class RegisterCoursesController {
                 }
             }
         }
-
         for(Course course: registerableCourses){
             for(Course Done: S1.getAllRegisteredCourses()){
                 if(course == Done){
