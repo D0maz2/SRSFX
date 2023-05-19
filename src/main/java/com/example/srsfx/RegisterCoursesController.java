e917d90490c0e0fa2d1cd39f15efc5ecaed7ee33
D0ma2 <adham.mohamed.elmahdy@gmail.com>
2023-05-19 15:24:10 +0300

    Adham

--- src/main/java/com/example/srsfx/RegisterCoursesController.java
+++ src/main/java/com/example/srsfx/RegisterCoursesController.java
@@ -16,6 +16,7 @@ import mainClasses.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RegisterCoursesController {
     private static String studentID;
@@ -52,10 +53,7 @@ public class RegisterCoursesController {
         String[] csFaculties = {"John Smith", "Jane Doe"};
         Department csDepartment = new Department(1001, "Computer Science", csFaculties);
         Student johnDoe = new Student(1001, "John Doe", "1999-01-01", "123 Main St", "555-1234", 2022, "Fall", new ArrayList<Course>(), new ArrayList<Course>(), csDepartment, 3.4, 3.5);
-
-
-
-
+        
         allStudents.add();
 
         filterCourses(mainClasses.allStudents.getStudentByID(studentID));
@@ -98,5 +96,4 @@ public class RegisterCoursesController {
             }
         }
     }
-
 }
